2296293acb7a1bb88b3b8db6181a52d9c241d74a
salina114406-ship-it <salina.114406@mitnepal.edu.np>
2026-08-18 20:06:10 +0545

    Delete java directory

--- java/sum.java.java
+++
@@ -1,9 +0,0 @@
-public class Calculate{
-   public static void main(String[] args){
-	int a = 10;
-	int b = 20;
-	int result = 0;
-	result = a+b;
-	System.out.println("Sum of "+a+" and "+b+" is: "+result);
-   }
-}
