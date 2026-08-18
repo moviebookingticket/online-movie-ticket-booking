e0eb755106b9f0433b3db0bbbeda74fad00a7ad8
salina114406-ship-it <salina.114406@mitnepal.edu.np>
2026-08-18 20:01:25 +0545

    Test cases

---
+++ java/sum.java.java
@@ -0,0 +1,9 @@
+public class Calculate{
+   public static void main(String[] args){
+	int a = 10;
+	int b = 20;
+	int result = 0;
+	result = a+b;
+	System.out.println("Sum of "+a+" and "+b+" is: "+result);
+   }
+}
